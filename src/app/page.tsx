3781a479e4ffd2d7121f7095282dca6a62e529b9
bear320 <aaabear320@gmail.com>
2024-11-14 20:02:32 +0800

    feat: 🎸 Setup for design system, theming & tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="text-3xl">
-      <h1>Store It - The only storage solution you need.</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">Store It - The only storage solution you need.</h1>
     </div>
   );
 }
