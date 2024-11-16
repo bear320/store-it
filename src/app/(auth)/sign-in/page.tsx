a872b60b5e9f3f02202061ac9c5aeb5a011e0c76
bear320 <aaabear320@gmail.com>
2024-11-16 13:53:19 +0800

    feat: 🎸 Create auth form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,11 @@
+import AuthForm from "@/components/AuthForm";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <AuthForm type="sign-in" />
+    </>
+  );
 };
 
 export default SignIn;
