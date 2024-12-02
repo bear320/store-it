57d2f3c6dada2aea077a16950ee6dffab3d6ef95
bear320 <aaabear320@gmail.com>
2024-12-02 21:54:28 +0800

    feat: 🎸 Set up Header & create Search and FileUploader

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Image from "next/image";
+import Search from "./Search";
+import FileUploader from "./FileUploader";
+import { Button } from "./ui/button";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Search />
+
+      <div className="header-wrapper">
+        <FileUploader />
+
+        <form>
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="Logout"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
