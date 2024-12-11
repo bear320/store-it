6d01e621c4bd0463f3bb3a8eff9cec34dc956e5f
bear320 <aaabear320@gmail.com>
2024-12-11 20:55:32 +0800

    feat: 🎸 Complete MobileNavigation

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -9,13 +9,12 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) return redirect("/sign-in");
-  console.log(currentUser);
 
   return (
     <main className="flex h-screen">
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation />
+        <MobileNavigation {...currentUser} />
         <Header />
 
         <div className="main-content">{children}</div>
